dec5bf834150c9abd89258007cf1c918a0506057
治 张 <zzdxzhangzhi@msn.com>
2017-05-08 21:29:05 +1200

    first commit

--- CsharpApp1/App.xaml.cs
+++ CsharpApp1/App.xaml.cs
@@ -28,6 +28,9 @@ namespace CsharpApp1
         /// </summary>
         public App()
         {
+            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
+                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
+                Microsoft.ApplicationInsights.WindowsCollectors.Session);
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
